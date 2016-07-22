62b06b4fe35e93dbc63d563b184ffacc0ee5f16d
zpq <307154456@qq.com>
2016-07-22 17:26:31 +0800

    add note web app

---
+++ noteweb/components/App.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div class="">
+        <side-bar></side-bar>
+        <notes-list></notes-list>
+        <editor></editor>
+    </div>
+</template>
+
+<script>
+    import SideBar from './SideBar.vue'
+    import NotesList from './NotesList.vue'
+    import Editor from './Editor.vue'
+
+    export default {
+        components: {
+            SideBar,
+            NotesList,
+            Editor,
+        }
+    }
+</script>
+
+<style lang="css">
+</style>
---
+++ noteweb/components/SideBar.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
