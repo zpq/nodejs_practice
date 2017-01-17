292ec74a2c5a190471695b4143c6f70bca045cbf
zhaopanqi <zhaopanqi@china-qizhi.com>
2017-01-17 17:28:54 +0800

    add my vue notebook

---
+++ my_vue_notebook/src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h1 class="text-center">elapsed time</h1>
+        </div>
+        <div class="panel-body">
+            <div class="text-center">
+                {{ time }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            time() {
+                return this.$store.state.totalTime
+            }
+        }
+    }
+</script>
