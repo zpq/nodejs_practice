3dac7526110a2085d2b67ec358be9e50b304a172
zhaopanqi <zhaopanqi@china-qizhi.com>
2019-01-29 17:43:31 +0800

    test

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,23 +1,25 @@
 var cheerio = require("cheerio");
 var http = require("http");
 var fs = require("fs");
-var uuid = require('node-uuid');
+var path = require('path');
 
 //var url = "http://sports.btime.com/others/20160610/n206752.shtml";
 // var url = "http://tieba.baidu.com/p/4608122171";
-
+//test
 var prefix = "http://www.yixiuba.com/BEAUTYLEGtuimo/2016/7160";
+prefix = "http://sports.btime.com/others/20160610/n206752.shtml";
+var dir = __dirname + "\\imgs\\";
+console.log(dir)
 var url;
+var range = process.argv[2] || 10;
 
-var range = process.argv[2] || 1;
-
-for (var i = 1;i <= range; i++) {
-	if (i == 1) {
-		url = prefix + ".html"; 
-	} else {
-		url = prefix + "_"+ i +".html"; 
-	}
-	
+for (var i = 1; i <= range; i++) {
+	// if (i == 1) {
+	// 	url = prefix + ".html"; 
+	// } else {
+	// 	url = prefix + "_" + i + ".html"; 
+	// }
+	url = prefix
 	http.get(url, function(res) {
 		var html = '';
 		res.on("data", function(data) {
@@ -27,9 +29,7 @@ for (var i = 1;i <= range; i++) {
 		res.on("end", function() {
 			$ = cheerio.load(html);
 			var imgs = $("img");
-			// console.log(imgs);
 			for (var i = 0; i < imgs.length; i++) {
-				// console.log(imgs.eq(i).attr("src"));
 				download(imgs.eq(i).attr("src"));
 			};
 		});
@@ -50,9 +50,8 @@ function download(imgurl) {
 			var filename = tmp[0];
 			tmp = filename.split("/");
 			filename = tmp[tmp.length - 1];
-			// console.log(filename);
-			// filename = uuid.v1() + ".jpg";
-			fs.writeFile("F:/imgs/nodejs/" + filename, imgData, "binary", function(err) {
+
+			fs.writeFile(dir + filename, imgData, "binary", function(err) {
 				if(err) {
 					console.log(err);
 				} else {
